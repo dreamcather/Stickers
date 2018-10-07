7ca0038b0f5e3dfd2f6044893245504f9611c0e2
evginiy <dreamcather1997@yandex.ru>
2018-10-07 14:51:22 +0300

    add registration name

--- src/main/java/Bridge.java
+++ src/main/java/Bridge.java
@@ -4,4 +4,8 @@ import java.util.ArrayList;
 
 public interface Bridge extends Remote {
     final String NAME = "Bridge";
+
+    public int addName(String name) throws RemoteException;
+
+    public boolean isExist(String name) throws RemoteException;
 }
--- src/main/java/BridgeClass.java
+++ src/main/java/BridgeClass.java
@@ -1,8 +1,25 @@
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
 
 public class BridgeClass extends UnicastRemoteObject implements Bridge {
 
+    private ArrayList<String> accounts;
     protected BridgeClass() throws RemoteException {
+        accounts = new ArrayList<>();
+    }
+
+    public int addName(String name) throws RemoteException {
+        if(!isExist(name)){
+            accounts.add(name);
+            return 0;
+        }
+        return -1;
+    }
+
+    public boolean isExist(String name) throws RemoteException {
+        if(accounts.indexOf(name)==-1)
+            return false;
+        return true;
     }
 }
